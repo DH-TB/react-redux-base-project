b961cda65539010eb4b6c6c68e8d05ba15a48af4
DH-TB <929659475@qq.com>
2016-10-15 13:33:07 +0800

    refactor add-todo

--- src/containers/add-todo.js
+++ src/containers/add-todo.js
@@ -3,16 +3,10 @@ import AddTodo from '../components/add-todo.jsx';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 
-function mapStateToProps(state) {
-  return {
-    todos: state.todos
-  };
-}
-
 function mapDispatchToProps(dispatch) {
   return {
     onAdd: (todos) => dispatch(actions.onAdd(todos))
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
+export default connect(()=>{return {}}, mapDispatchToProps)(AddTodo);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,5 @@
 import actionTypes from '../actions/types';
 
-
-function _handleDelete(state, action) {
-  return Object.assign({}, state, {
-    fruit: state.fruit.filter(name => name !== action.fruitName)
-  });
-}
-
 function _handleAdd(state, action) {
   state.todos.push(action.todos);
   return state;
@@ -14,8 +7,6 @@ function _handleAdd(state, action) {
 
 export default function (state = {todos: []}, action) {
   switch (action.type) {
-    case actionTypes.DELETE_FRUIT:
-      return _handleDelete(state, action);
     case actionTypes.ADD:
       return _handleAdd(state, action);
     default:
